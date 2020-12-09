467ea8c87ae1fc952b339f71b73e2704692c0da9
nisol91 <nicolasolzi91@gmail.com>
2020-12-09 19:29:22 +0100

    styling blog

--- src/components/metamorphosis/MBlog.vue
+++ src/components/metamorphosis/MBlog.vue
@@ -6,7 +6,45 @@
         v-for="(post, i) in blogPosts"
         :key="i + `_blogPost`"
       >
-        {{ post.title }}
+        <div class="pSx">
+          <div class="pCategory">
+            {{ post.category }}
+          </div>
+          <div class="pTitle">
+            {{ post.title }}
+          </div>
+          <div class="pSubTitle">
+            {{ post.subtitle }}
+          </div>
+          <div class="pDate">
+            {{
+              new Date(post.createdTimestamp.seconds * 1000)
+                | moment("dddd, MMMM Do YYYY")
+            }}
+          </div>
+          <div class="pTags">
+            <div class="pTag" v-for="(tag, i) in post.tags" :key="i + `_tag`">
+              {{ tag }}
+            </div>
+          </div>
+        </div>
+        <div class="pDx">
+          <v-img
+            v-if="post.media"
+            :src="post.media[0]"
+            class="grey lighten-2 pMedia"
+            :aspect-ratio="16 / 9"
+          >
+            <template v-slot:placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+        </div>
       </div>
     </div>
     <div class="blogFilters">dx</div>
@@ -15,6 +53,7 @@
 <script>
 import { db, Timestamp, GeoPoint } from "../../main";
 import firebase from "firebase";
+import VueMoment from "vue-moment";
 
 export default {
   data() {
@@ -81,7 +120,57 @@ export default {
   border-radius: 3px;
   margin: 10px;
   display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 20px;
+}
+.pTitle {
+  font-weight: bold;
+  font-size: 30px;
+  cursor: pointer;
+  transition: 0.5s;
+  padding: 5px;
+
+  &:hover {
+    background: rgb(194, 194, 194);
+    transition: 0.5s;
+    border-radius: 3px;
+  }
+}
+.pSx {
+  width: 30%;
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
+  flex-direction: column;
+}
+.pDx {
+  width: 70%;
+  height: 100%;
+  display: flex;
   justify-content: center;
   align-items: center;
+}
+.pSubtitle {
+  font-size: 25px;
+}
+.pMedia {
+  width: 100%;
+  border-radius: 4px;
+}
+.pCategory {
+  font-style: italic;
+}
+.pDate {
+  font-weight: 700;
+  font-size: 12px;
+}
+.pTags {
+  width: 80%;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
 }
 </style>
